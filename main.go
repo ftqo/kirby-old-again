1431b5b4279b46619e54c55d4834f854759b994f
fittsqo <fittsqo@gmail.com>
2021-12-19 15:32:25 -0800

    initial commit

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var (
+	GuildID  = flag.String("guild", "", "Test guild ID. If not passed - bot registers commands globally")
+	BotToken = flag.String("token", "", "Bot access token")
+)
+
+var s *discordgo.Session
+
+func init() {
+	flag.Parse()
+}
+
+func init() {
+	var err error
+	s, err = discordgo.New("Bot " + *BotToken)
+	if err != nil {
+		log.Fatalf("Invalid bot parameters: %v", err)
+	}
+}
+
+func main() {
+	s.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) { log.Println("Bot is up!") })
+
+	err := s.Open()
+	if err != nil {
+		log.Fatalf("Cannot open the session: %v", err)
+	}
+	defer s.Close()
+
+	stop := make(chan os.Signal)
+	signal.Notify(stop, os.Interrupt)
+	<-stop
+	log.Println("Gracefully shutting down")
+}

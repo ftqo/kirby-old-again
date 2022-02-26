e91dada5a8151cd1d075ad34d787bd0389bbcc6b
ftqo <fittsqo@gmail.com>
2022-02-26 12:00:56 -0800

    added logging to a logs folder

--- logger/logger.go
+++ logger/logger.go
@@ -1,7 +1,11 @@
 package logger
 
 import (
+	"io"
 	"os"
+	"path"
+	"runtime"
+	"time"
 
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
@@ -10,5 +14,19 @@ import (
 var L zerolog.Logger
 
 func init() {
-	L = log.Output(zerolog.ConsoleWriter{Out: os.Stdout})
+	_, b, _, _ := runtime.Caller(0)
+	d := path.Join(path.Dir(b))
+	logsPath := path.Join(d, "../logs")
+	_, err := os.Open(logsPath)
+	if err != nil {
+		err = os.Mkdir(logsPath, 0755)
+		if err != nil {
+			panic("unable to create logs folder: " + err.Error())
+		}
+	}
+	fc, err := os.Create(logsPath + "/" + time.Now().Format("2006-01-02-15:04:05") + ".log")
+	if err != nil {
+		panic("failed to initialize logger: " + err.Error())
+	}
+	L = log.Output(zerolog.ConsoleWriter{Out: io.MultiWriter(os.Stdout, fc)})
 }

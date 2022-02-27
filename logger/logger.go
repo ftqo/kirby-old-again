3bbbd4cedf5373ab4411d6e6ad1c0316cb1aef49
ftqo <fittsqo@gmail.com>
2022-02-26 23:16:40 -0800

    implemented compression for logs

--- logger/logger.go
+++ logger/logger.go
@@ -1,6 +1,7 @@
 package logger
 
 import (
+	"compress/gzip"
 	"io"
 	"os"
 	"path"
@@ -13,6 +14,7 @@ import (
 
 var L zerolog.Logger
 var f *os.File
+var fp string
 
 func Initialize() {
 	_, b, _, _ := runtime.Caller(0)
@@ -22,22 +24,36 @@ func Initialize() {
 	if err != nil {
 		err = os.Mkdir(logsPath, 0o755)
 		if err != nil {
-			panic("Unable to create logs folder: " + err.Error())
+			panic("Failed to create logs folder: " + err.Error())
 		}
 	}
-	f, err = os.Create(logsPath + "/" + time.Now().Format("2006-01-02--15-04-05") + ".log")
+	fp = logsPath + "/" + time.Now().Format("2006-01-02--15-04-05") + ".log"
+	f, err = os.Create(fp)
 	if err != nil {
-		panic("Failed to initialize logger: " + err.Error())
+		panic("Failed to create logs file: " + err.Error())
 	}
 	L = log.Output(zerolog.ConsoleWriter{Out: io.MultiWriter(os.Stdout, f)})
 	L.Info().Msg("Initialized logger")
 }
 
 func Close() {
-	L.Info().Msg("Closing logger. THIS SHOULD BE THE FINAL LOG MESSAGE!")
-	err := f.Close()
+	L.Info().Msg("Closing logger and compressing logs; THIS SHOULD BE THE FINAL LOG MESSAGE")
+	cf, err := os.Create(fp + ".gz")
 	if err != nil {
-		// UHHHHHHHHHHHHHHH someoneplstellmewhattodohere
-		L.Panic().Err(err).Msg("Failed to close logger")
+		L.Error().Err(err).Msg("Failed to create file for compressed logs; keeping non-compressed logs")
+		f.Close()
+		return
+	}
+	gz := gzip.NewWriter(cf)
+	f.Close()
+	f, _ = os.Open(fp)
+	_, err = io.Copy(gz, f)
+	gz.Close()
+	if err != nil {
+		L.Error().Err(err).Msg("Failed to compress logs; keeping non-compressed logs")
+		f.Close()
+	} else {
+		f.Close()
+		os.Remove(fp)
 	}
 }
